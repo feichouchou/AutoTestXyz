3a289c4f1fda5693a2f57152334ca71e0dbdeb66
gushuang <gushuang5210@163.com>
2022-01-17 15:09:44 +0800

    删除appium相关配置文件

--- appium-inspector-main/app/renderer/actions/index.js
+++
@@ -1,9 +0,0 @@
-import * as inspectorActions from './Inspector';
-import * as sessionActions from './Session';
-import * as updaterActions from '../../../gui-common/actions/Updater';
-
-export default {
-  ...inspectorActions,
-  ...sessionActions,
-  ...updaterActions,
-};
